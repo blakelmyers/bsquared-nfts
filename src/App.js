ef0a1d57ba728c449c215fa0d6d8d84c5321f9ef
Blake Myers <blakelmyers@gmail.com>
2022-01-02 22:28:04 -0600

    added signup form

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -22,8 +22,22 @@ class App extends React.Component {
 
   componentDidMount() {
     this.unsubscribeFromAuth =
-      auth.onAuthStateChanged(user => {
-        this.setState({ currentUser: user });
+      auth.onAuthStateChanged(async userAuth => {
+        if (userAuth) {
+            const userRef = await createUserProfileDocument(userAuth);
+
+            userRef.onSnapshot(snapShop => {
+              this.setState({
+                currentUser: {
+                  id: snapShop.id,
+                  ...snapShop.data()
+                }
+              })
+            })
+        }
+        else {
+          this.setState({ currentUser: userAuth });
+        }
      })
   }
 
