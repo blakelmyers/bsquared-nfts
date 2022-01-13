8bcc7b8f15722ce5f9e563d4b956f402484e120c
Blake Myers <blakelmyers@gmail.com>
2022-01-13 00:05:31 -0600

    Adding Redux to project

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -1,61 +1,61 @@
 const SHOP_DATA = [
     {
       id: 1,
-      title: 'Hats',
+      title: 'Cats',
       routeName: 'hats',
       items: [
         {
           id: 1,
-          name: 'Brown Brim',
-          imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+          name: 'Green Cat',
+          imageUrl: 'https://i.imgur.com/DO4iuPv.jpg',
           price: 25
         },
         {
           id: 2,
-          name: 'Blue Beanie',
-          imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+          name: 'Red Cat',
+          imageUrl: 'https://i.imgur.com/TMKGUyY.jpg',
           price: 18
         },
         {
           id: 3,
-          name: 'Brown Cowboy',
-          imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+          name: 'Moon Cat',
+          imageUrl: 'https://i.imgur.com/IhBCalu.jpg',
           price: 35
         },
         {
           id: 4,
-          name: 'Grey Brim',
-          imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
+          name: 'Orange Cat',
+          imageUrl: 'https://i.imgur.com/Vj3Zz8n.jpg',
           price: 25
         },
         {
           id: 5,
-          name: 'Green Beanie',
-          imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
+          name: 'Party Cat',
+          imageUrl: 'https://i.imgur.com/qG0XU38.jpg',
           price: 18
         },
         {
           id: 6,
-          name: 'Palm Tree Cap',
-          imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
+          name: 'Pink Cat',
+          imageUrl: 'https://i.imgur.com/o07QTcn.jpg',
           price: 14
         },
         {
           id: 7,
-          name: 'Red Beanie',
-          imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
+          name: 'Yellow Cat',
+          imageUrl: 'https://i.imgur.com/FWyS1Zu.jpg',
           price: 18
         },
         {
           id: 8,
-          name: 'Wolf Cap',
-          imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
+          name: 'Blue Cat',
+          imageUrl: 'https://i.imgur.com/54Vvc9n.jpg',
           price: 14
         },
         {
           id: 9,
-          name: 'Blue Snapback',
-          imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
+          name: 'Pink Cat',
+          imageUrl: 'https://i.imgur.com/18HWYQb.jpg',
           price: 16
         }
       ]
