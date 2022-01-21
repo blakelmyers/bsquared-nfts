1d100439885b18c50b91bbabda0f22110cdc402d
Blake Myers <blakelmyers@gmail.com>
2022-01-20 23:54:46 -0600

    Add data to firebase and fixed page links

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+
 import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 import MenuItem from '../menu-item/menu-item.component';
-import './directory.styles.scss';
 
-const Directory = ({ sections }) => (
-      <div className='directory-menu'>
-          {
-            sections.map(({title, imageUrl, id, size}) => (
-                 <MenuItem key={id} size={size} title={title} imageUrl={imageUrl}/>
-            ))
-          }
-      </div>
+import { DirectoryMenuContainer } from './directory.styles';
+
+export const Directory = ({ sections }) => (
+  <DirectoryMenuContainer>
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </DirectoryMenuContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
